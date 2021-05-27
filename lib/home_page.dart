99c2d5358c2d36e9a92e63cd2cd9b44f467a4d7b
21SUSANTI <susantilalala@gmail.com>
2021-05-28 00:56:29 +0700

    Membuat button setelah login

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,54 +1,79 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   static String tag = 'home-page';
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  TabController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = TabController(vsync: this, length: 3);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    final alucard = Hero(
-      tag: 'hero',
-      child: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: CircleAvatar(
-          radius: 72.0,
-          backgroundColor: Colors.transparent,
-          backgroundImage: AssetImage('assets/tartan.jpeg'),
+    return new Scaffold(
+      //create appBar
+      appBar: new AppBar(
+        //warna background
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search, color: Colors.white),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.shopping_cart, color: Colors.white),
+            onPressed: () {},
+          ),
+        ],
+        title: Text("NONA'S"),
+        centerTitle: true,
+      ),
+
+      bottomNavigationBar: Material(
+        color: Colors.white,
+        child: TabBar(
+          controller: controller,
+          tabs: <Widget>[
+            GestureDetector(
+                onTap: () {},
+                child: Tab(
+                  icon: Icon(
+                    Icons.home,
+                    color: Colors.brown,
+                  ),
+                )),
+            Tab(
+              child: Icon(
+                Icons.favorite,
+                color: Colors.brown,
+              ),
+            ),
+            Tab(
+              child: Icon(
+                Icons.person,
+                color: Colors.brown,
+              ),
+            ),
+          ],
         ),
       ),
-    );
-
-    final welcome = Padding(
-      padding: EdgeInsets.all(8.0),
-      child: Text(
-        'Welcome NonaS',
-        style: TextStyle(fontSize: 28.0, color: Colors.white),
-      ),
-    );
-
-    final lorem = Padding(
-      padding: EdgeInsets.all(8.0),
-      child: Text(
-        'aaaaaaaaaaaaaa',
-        style: TextStyle(fontSize: 16.0, color: Colors.white),
-      ),
-    );
-
-    final body = Container(
-      width: MediaQuery.of(context).size.width,
-      padding: EdgeInsets.all(28.0),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(colors: [
-          Colors.brown,
-          Colors.white,
-        ]),
-      ),
-      child: Column(
-        children: <Widget>[alucard, welcome, lorem],
-      ),
-    );
-
-    return Scaffold(
-      body: body,
     );
   }
 }
