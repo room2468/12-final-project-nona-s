c6a97e18ff7087680e971ef04cd916407e9c3ac1
SUSANTI <64391805+21SUSANTI@users.noreply.github.com>
2021-06-20 23:17:49 +0700

    Membuat convert pembayaran agar otomatis

--- lib/convert.dart
+++ lib/convert.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class Convert extends StatelessWidget {
+  final Function konvertHandler;
+
+  Convert({Key key, @required this.konvertHandler});
+
+//function yg digunakan untuk melakukan serstate()
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+    );
+  }
+}
