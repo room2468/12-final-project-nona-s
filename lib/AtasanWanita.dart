bf2bdfef87f89c7a14227320aefabc6598b40dc9
21SUSANTI <susantilalala@gmail.com>
2021-06-20 22:49:26 +0700

    Membuat isi kategori baju Wanita

---
+++ lib/AtasanWanita.dart
@@ -0,0 +1,175 @@
+import 'package:flutter/material.dart';
+//import 'package:nonas/Pembayaran.dart';
+
+class AtasanWanitaPage extends StatefulWidget {
+  static String tag = 'AtasanWanita-page';
+  @override
+  _AtasanWanitaPageState createState() => new _AtasanWanitaPageState();
+}
+
+class _AtasanWanitaPageState extends State<AtasanWanitaPage> {
+  @override
+  Widget build(BuildContext context) {
+    final bar = AppBar(
+      elevation: .5,
+      leading: IconButton(
+        icon: Icon(Icons.arrow_back, color: Colors.white),
+        onPressed: () => Navigator.of(context).pop(),
+      ),
+      title: Text("FIND HERE"),
+      actions: <Widget>[
+        IconButton(
+            icon: Icon(Icons.search, color: Colors.white),
+            onPressed: () {
+              showSearch(context: context, delegate: DataSearch());
+            }),
+        IconButton(
+          icon: Icon(Icons.shopping_cart, color: Colors.white),
+          onPressed: () {},
+        ),
+      ],
+    );
+
+    final search = Hero(
+      tag: 'hero',
+      child: Padding(
+        padding: EdgeInsets.all(15.0),
+        child: CircleAvatar(
+          radius: 70.0,
+          backgroundImage: AssetImage('assets/tartan.jpeg'),
+        ),
+      ),
+    );
+
+    final paragraf1 = Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Text(
+        'Temukan kode produk atasan wanita yang akan dibeli',
+        textAlign: TextAlign.center,
+        style: TextStyle(fontSize: 20.0, color: Colors.white),
+      ),
+    );
+
+    final body = Container(
+      width: MediaQuery.of(context).size.width,
+      padding: EdgeInsets.all(28.0),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(colors: [
+          Colors.brown,
+          Colors.brown,
+        ]),
+      ),
+      child: Column(
+        children: <Widget>[bar, search, paragraf1],
+      ),
+    );
+
+    return Scaffold(
+      body: body,
+    );
+  }
+}
+
+class DataSearch extends SearchDelegate<String> {
+  final atasanWanita = ["AW1", "AW2", "AW3", "AW4", "AW5", "AW6"];
+
+  final recentAtasanWanita = ["AW1", "AW2", "AW3", "AW4", "AW5", "AW6"];
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+          icon: Icon(Icons.clear),
+          onPressed: () {
+            query = "";
+          })
+    ];
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Detail pencarian',
+      home: Scaffold(
+          body: Column(children: <Widget>[
+        Image(
+          image: AssetImage('assets/tartan7.jpeg'),
+        ),
+        Text(
+          'Detail Produk Atasan Wanita',
+          style: TextStyle(fontSize: 24, fontFamily: "Serif", height: 2.0),
+        ),
+        Text('By NONAS'),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text(
+              '\t\t\t\t\tKategorit\t\t\t\t\t: Atasan Wanita\n\t\t\t\t\tMerk\t\t\t\t\t: Zara\n\t\t\t\t\tBahan\t\t\t\t\t: Katun\n\t\t\t\t\tStock\t\t\t\t\t:1000\n\t\t\t\t\tDikirim Dari\t\t\t\t:Malang',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 20, fontFamily: "Serif", height: 2.0),
+            ),
+          ],
+        ),
+        Container(
+          width: double.infinity,
+          height: 50,
+          // ignore: deprecated_member_use
+          child: RaisedButton(
+            color: Colors.brown,
+            child: Text(
+              "BELI SEKARANG",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onPressed: () {
+              //Navigator.of(context).pushNamed(Pembayaran.tag);
+            },
+          ),
+        ),
+      ])),
+    );
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+        icon: AnimatedIcon(
+          icon: AnimatedIcons.menu_arrow,
+          progress: transitionAnimation,
+        ),
+        onPressed: () {
+          close(context, null);
+        });
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    final suggestionList = query.isEmpty
+        ? recentAtasanWanita
+        : atasanWanita.where((p) => p.startsWith(query)).toList();
+
+    return ListView.builder(
+      itemBuilder: (context, index) => ListTile(
+        onTap: () {
+          showResults(context);
+        },
+        leading: Icon(Icons.location_city),
+        title: RichText(
+          text: TextSpan(
+              text: suggestionList[index].substring(0, query.length),
+              style:
+                  TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+              children: [
+                TextSpan(
+                    text: suggestionList[index].substring(query.length),
+                    style: TextStyle(color: Colors.grey))
+              ]),
+        ),
+      ),
+      itemCount: suggestionList.length,
+    );
+  }
+}
