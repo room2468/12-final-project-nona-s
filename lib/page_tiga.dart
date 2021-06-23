ea99c65c3b218771a79627a7e8776079920cfcfd
SUSANTI <64391805+21SUSANTI@users.noreply.github.com>
2021-06-24 00:08:05 +0700

    Update Halaman Profil dan Instagram

--- lib/page_tiga.dart
+++ lib/page_tiga.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:nonas/InstagramPage.dart';
+import 'package:nonas/login_page.dart';
 
 class PageTiga extends StatelessWidget {
   @override
@@ -34,16 +36,50 @@ class PageTiga extends StatelessWidget {
     final paragraf2 = Padding(
       padding: EdgeInsets.all(8.0),
       child: Text(
-        'NonaS Group adalah sebuah  e-commerce  yang didirikan oleh Novelya, Nafhida, dan Susanti pada 2020, guna memenuhi nilai projek akhir pada  Mata Kuliah Pemrograman Mobile',
+        'NonaS Group adalah sebuah  e-commerce  yang didirikan oleh Novelya, Nafhida, dan Susanti pada tahun 2021, guna memenuhi nilai projek akhir pada  Mata Kuliah Pemrograman Mobile. NonaS Group menyediakan berbagai macam fashion kekinian.',
         style: TextStyle(fontSize: 16.0, color: Colors.white),
       ),
     );
 
-    final paragraf3 = Padding(
-      padding: EdgeInsets.all(8.0),
-      child: Text(
-        'NonaS Group menyediakan berbagai macam product dengan bahan dasar kain Tartan yang unik dan indah, terbagi dalam berbagai kategori pria dan wanita',
-        style: TextStyle(fontSize: 16.0, color: Colors.white),
+    final paragraf3 = Container(
+      width: double.infinity,
+      // ignore: deprecated_member_use
+      child: RaisedButton(
+        color: Colors.redAccent,
+        child: Text(
+          "Instagram NONAS",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        onPressed: () {
+          Navigator.of(context).pushNamed(InstagramPage.tag);
+        },
+      ),
+    );
+
+    final paragraf4 = Container(
+      margin: EdgeInsets.all(15),
+      width: double.infinity,
+      height: 40,
+      // ignore: deprecated_member_use
+      child: MaterialButton(
+        color: Colors.white,
+        height: 40,
+        child: Text(
+          "Log Out",
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => LoginPage()));
+        },
       ),
     );
 
@@ -57,7 +93,14 @@ class PageTiga extends StatelessWidget {
         ]),
       ),
       child: Column(
-        children: <Widget>[profil, welcome, paragraf1, paragraf2, paragraf3],
+        children: <Widget>[
+          profil,
+          welcome,
+          paragraf1,
+          paragraf2,
+          paragraf3,
+          paragraf4
+        ],
       ),
     );
 
