cc1b815318ec85b3da3e0cf5ecc153d8268e97e6
21SUSANTI <susantilalala@gmail.com>
2021-06-23 22:03:01 +0700

    Isi dari URL Instagram

---
+++ lib/InstagramPage.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:nonas/Instagram.dart';
+
+class InstagramPage extends StatefulWidget {
+  static String tag = 'AtasanPria-page';
+  @override
+  _InstagramPageState createState() => new _InstagramPageState();
+}
