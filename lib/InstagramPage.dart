1303c0646b8be8aeecdae720bd4311e92daa72a9
21SUSANTI <susantilalala@gmail.com>
2021-06-23 23:55:52 +0700

    update instagram

--- lib/InstagramPage.dart
+++ lib/InstagramPage.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:nonas/Instagram.dart';
 
 class InstagramPage extends StatefulWidget {
-  static String tag = 'AtasanPria-page';
+  static String tag = 'InstagramPage-page';
   @override
   _InstagramPageState createState() => new _InstagramPageState();
 }
